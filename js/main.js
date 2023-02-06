6f069093d01f438141319f1fa3851d5949678018
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2023-02-06 13:25:54 +0300

    new update

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
+// loader at the opening of website
 setTimeout(function(){
     document.getElementById("loader").style.display = "none";
     document.getElementById("main-content").style.display = "block";
 }, 1000);
-
+// open side bar logic
 const revealSideBar = document.querySelector('.side-bar');
 const menuIcon = document.querySelector('.icon');
 menuIcon.addEventListener('click', function (){
     revealSideBar.style.display = 'block'
 });
-
+// close sidebar logic
 const close = document.querySelector('.side-bar');
 const closeIcon = document.querySelector('#close-window');
 closeIcon.addEventListener('click', function (){
     close.style.display = 'none'
 });
-
+// sign in password tester
 const loginFirstName = document.querySelector('.first-login-page');
 const Page2  = document.querySelector('.second-page')
 const signBtn = document.querySelector('.sign-up-btn');
 const UserName = document.querySelector('.userName');
 const profileName = document.querySelector('#profile');
 const phoneNumber = document.querySelector('.phone-no');
-
-
-
 
 signBtn.addEventListener('click', function (){
     if (UserName.value == 0){
@@ -52,7 +50,7 @@ signBtn.addEventListener('click', function (){
     } 
 });
 
-
+// toggle between register and sign in
 const signInForm = document.querySelector('#sign-in');
 const registerForm  = document.querySelector('#register')
 const createAccountBtn = document.querySelector('#account');
@@ -61,11 +59,6 @@ createAccountBtn.addEventListener('click', function (){
     signInForm.style.display = 'none'
     registerForm.style.display = 'block'
 });
-// signBtn.addEventListener('click', function (){
-//     signInForm.style.display = 'block'
-//     registerForm.style.display = 'none'
-// });
-
 
 //  password validation tester
 var password = document.getElementById('passWord');
@@ -73,7 +66,6 @@ var border = document.getElementsByTagName('input');
 var msg = document.getElementById('message');
 var str = document.getElementById('strength');
 var confirmPassword = document.getElementById('confirm-passWord');
-
 
 password.addEventListener('input',()=>{
 if (password.value.length > 0){
@@ -100,8 +92,8 @@ else if(password.value.length >= 12){
 });
 confirmPassword.addEventListener('click', function(){
     msg.style.display = 'none'
-})
-
+});
+// password tester when registering
 const RegisterButton = document.querySelector('.register-btn');
 const RegisterBTN = document.querySelector('#register-btn');
 const confirmError = document.querySelector('.confirmation-error');
@@ -109,11 +101,8 @@ const firstName = document.querySelector('#firstName');
 const secondName = document.querySelector('#secondName');
 const phonenumber = document.querySelector('#phoneNumber');
 
-
 RegisterButton.addEventListener('click',function(){
-    // if(password.value == confirmPassword.value){
-    //     return true;
-    // }
+  
     if(firstName.value == 0){
         document.querySelector('.first-name-error').innerHTML = 'First Name Is Required';
         firstName.style.borderColor = 'red';
@@ -150,10 +139,123 @@ RegisterButton.addEventListener('click',function(){
         });
     }
 });
+// a graph to show the recent data transmitted
+$(document).ready(function() {
+$.ajax({
+    type: "GET",
+    url: "network_data.csv",
+    dataType: "text",
+    success: function(data) {
+    var allLines = data.split(/\r?\n|\r/);
+    var headers = allLines[0].split(',');
+    var rbData = [];
+    var tbData = [];
+    for (var i = 1; i < allLines.length; i++) {
+        var data = allLines[i].split(',');
+        rbData.push(data[1]);
+        tbData.push(data[4]);
+    }
+    var ctx = document.getElementById('chart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+        labels: Array.from({length: rbData.length}, (_, i) => i + 1),
+        datasets: [
+            {
+            label: 'received bytes',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            data: rbData
+            },
+            {
+            label: 'transmitted bytes',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            data: tbData
+            }
+        ]
+        },
+        options: {
+        scales: {
+            yAxes: [
+            {
+                id: 'rb-axis',
+                position: 'left',
+                gridLines: {
+                color: 'rgba(0, 0, 0, 0.1)'
+                }
+            },
+            {
+                id: 'tb-axis',
+                position: 'right',
+                gridLines: {
+                color: 'rgba(0, 0, 0, 0.1)'
+                }
+            }
+            ]
+        }
+        }
+    });
+    }
+});
+});
+// toggle change graph logic
+const changeGraph = document.querySelector('.change-graph');
+const hideSideBarList = document.querySelector('#hide');
+const revealGraphTypes = document.querySelector('.graph-types');
+const backNavigationArrow = document.querySelector('.back-navigation-arrow');
 
+changeGraph.addEventListener('click', function(){
+    hideSideBarList.style.display = 'none'
+    revealGraphTypes.style.display = 'block'
+    backNavigationArrow.style.display = 'block'
+});
+// toggle Back to sidebar list from graph list logic 
+backNavigationArrow.addEventListener('click', function(){
+    revealGraphTypes.style.display = 'none'
+    hideSideBarList.style.display = 'block'
+    backNavigationArrow.style.display = 'none'
+});
+// hide everything else and show wifi usage table
+const allConnectedWifiData = document.querySelector('.all-connected-wifi-data');
+const tableHeaderForAllWifiData = document.querySelector('.table-header');
+const networkTable = document.querySelector('#network-table');
+const latestWifiUsage = document.querySelector('#chart');
+const Table = document.querySelector('.table');
+const AvailableBalanceBlock = document.querySelector('.available-bal');
+const analysisHeader = document.querySelector('.analysis');
 
+allConnectedWifiData.addEventListener('click', function(){
+    tableHeaderForAllWifiData.style.display = 'block'
+    networkTable.style.display = 'none'
+    latestWifiUsage.style.display = 'none'
+    Table.style.display = 'none'
+    analysisHeader.style.display = 'none'
+    AvailableBalanceBlock.style.display = 'none'
+});
+// fetch all the data from csv and display it in html table
+const table = document.getElementById("network-table");
 
+fetch("network_data.csv")
+.then(response => response.text())
+.then(data => {
+    const rows = data.split("\n");
+    const headers = rows[0].split(",");
 
+    let tableHTML = "";
+    tableHTML += "<tr>";
+    headers.forEach(header => {
+    tableHTML += `<th>${header}</th>`;
+    });
+    tableHTML += "</tr>";
 
-
-
+    for (let i = 1; i < rows.length; i++) {
+    const cells = rows[i].split(",");
+    tableHTML += "<tr>";
+    cells.forEach(cell => {
+        tableHTML += `<td>${cell}</td>`;
+    });
+    tableHTML += "</tr>";
+    }
+    table.innerHTML = tableHTML;
+});
